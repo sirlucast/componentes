ef784fc745788981c8bb37f2be354ffb61c4db22
sirlucast <jltorres@cntx.cl>
2020-04-27 17:17:19 -0400

    widget alert + widget avatar

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -13,6 +13,47 @@ class AlertPage extends StatelessWidget {
         child: Icon(Icons.backspace),
         onPressed: () => Navigator.pop(context),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("disparar alerta"),
+          onPressed: () => _mostrarAlerta(context),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+        ),
+      ),
+    );
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es  lcontenido de la caaj de alerta'),
+              FlutterLogo(size: 100),
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Cancelar'),
+            ),
+            FlatButton(
+              onPressed: () {},
+              child: Text('Ok'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,6 +8,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar pages'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(10),
+            child: CircleAvatar(
+              child: Text('LT'),
+              backgroundColor: Colors.greenAccent,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://i.pinimg.com/originals/3a/86/a5/3a86a5d7a7472d99365b4ffa01bd21bd.jpg'),
+              radius: 20,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage.assetNetwork(
+          placeholder: 'assets/original.gif',
+          image:
+              'https://eloutput.com/app/uploads-eloutput.com/2020/04/Batman.jpg',
+          fadeInDuration: Duration(milliseconds: 250),
+        ),
       ),
     );
   }
